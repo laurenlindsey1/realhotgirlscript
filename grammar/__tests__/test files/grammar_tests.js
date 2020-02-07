e4e1b0693bd4efeee9ceed251dca6300df4ae765
Sage Strieker <sspink157@gmail.com>
2020-02-06 19:59:04 -0800

    added js tests

---
+++ grammar/__tests__/test files/grammar_tests.js
@@ -0,0 +1,33 @@
+/*
+ * Grammar Tests
+ *
+ * Tests that the we've constructed our grammar correctly, by checking that
+ * programs that we expect to be matched by the grammar are matched, and
+ * those that we expect not to cause an error to be thrown.
+ */
+
+const fs = require("fs");
+const assert = require("assert");
+const parse = require("../../syntax/parser.js");
+
+describe("The grammar", () => {
+  fs.readdirSync(__dirname).forEach(name => {
+    if (name.endsWith(".hotgirl")) {
+      it(`matches the program ${name}`, done => {
+        fs.readFile(`${__dirname}/${name}`, "utf-8", (err, input) => {
+          // In this test we just care that it parses without errors
+          assert.ok(parse(input));
+          done();
+        });
+      });
+    } else if (name.endsWith(".error")) {
+      it(`detects a syntax error in ${name}`, done => {
+        fs.readFile(`${__dirname}/${name}`, "utf-8", (err, input) => {
+          // We always wrap Ohm failures in an error with text "Syntax Error"
+          assert.throws(() => parse(input), /Syntax Error/);
+          done();
+        });
+      });
+    }
+  });
+});
