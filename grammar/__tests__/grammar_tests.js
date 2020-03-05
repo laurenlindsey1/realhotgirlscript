dfd17d5eb9bf812756628e0394d58eaff86ea454
Shanaya Nagendran <shanaya.n@gmail.com>
2020-03-05 15:31:13 -0800

    WE GOT COVERAGE

--- grammar/__tests__/grammar_tests.js
+++ grammar/__tests__/grammar_tests.js
@@ -1,14 +1,29 @@
 const fs = require("fs");
 const assert = require("assert");
 const parse = require("../../syntax/parser.js");
-const TEST_DIR = `grammar/__tests__/testFiles/goodPrograms`;
+const TEST_DIR_GOOD = `grammar/__tests__/testFiles/goodPrograms`;
+const TEST_DIR_BAD = `grammar/__tests__/testFiles/badPrograms`;
 describe("The grammar", () => {
-  fs.readdirSync(TEST_DIR).forEach(name => {
+  fs.readdirSync(TEST_DIR_GOOD).forEach(name => {
     if (name.endsWith(".hotgirl")) {
       it(`matches the program ${name}`, done => {
-        fs.readFile(`${TEST_DIR}/${name}`, "utf-8", (err, input) => {
+        fs.readFile(`${TEST_DIR_GOOD}/${name}`, "utf-8", (err, input) => {
           // In this test we just care that it parses without errors
           assert.ok(parse(input));
+          done();
+        });
+      });
+    }
+  });
+});
+
+describe("The grammar", () => {
+  fs.readdirSync(TEST_DIR_BAD).forEach(name => {
+    if (name.endsWith(".error")) {
+      it(`detects a syntax error in ${name}`, done => {
+        fs.readFile(`${TEST_DIR_BAD}/${name}`, "utf-8", (err, input) => {
+          // We always wrap Ohm failures in an error with text "Syntax Error"
+          assert.throws(() => parse(input), /Syntax Error/);
           done();
         });
       });
