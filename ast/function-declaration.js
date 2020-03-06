6d94ceae57c3d2c3d1704cf8edbef53380ac9ea6
Strieker <Strieker@users.noreply.github.com>
2020-03-05 16:35:22 -0800

    removed an unnecessary comment!

--- ast/function-declaration.js
+++ ast/function-declaration.js
@@ -1,6 +1,5 @@
 module.exports = class FunctionDeclaration {
   constructor(async, type, id, params, body) {
-    // WHERE TO ADD VOID IN HERE?
     Object.assign(this, { async, type, id, params, body });
   }
 };
