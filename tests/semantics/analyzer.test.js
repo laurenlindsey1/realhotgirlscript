76831b8ccec6a7c88b84650ed91242bb302423ae
Lauren Lindsey <llindse3@lion.lmu.edu>
2020-04-12 20:50:14 -0700

    modify error outputs to align with checks

--- tests/semantics/analyzer.test.js
+++ tests/semantics/analyzer.test.js
@@ -19,9 +19,9 @@ const errors = {
   "function-call-with-incorrect-args": "Incorrect function call",
   "function-no-return": "Missing return statement",
   "if-else-repeat": "Incorrect if/else usage",
-  "if-not-boolz": "Types are not compatible",
+  "if-not-boolz": "Not a boolean",
   "incompatible-func-return-types": "Types are not compatible",
-  "incorrect-binary-exp": "Cannot be used in a binary expression",
+  "incorrect-binary-exp": "Not an integer",
   "incorrect-reassignments": "Types are not compatible",
   "loop-error": "Types are not compatible",
   "mismatched-assignment-length": "Incorrect number of arguments",
@@ -29,21 +29,21 @@ const errors = {
   "reused-var-declaration": "Identifier already declared in this scope",
   "setz-assigned-to-boolz": "Types are not compatible",
   "setz-improper": "Types are not compatible",
-  "stayz-reassignment": "Cannot reassign to constant value",
+  "stayz-reassignment": "Types are not compatible",
   "subscripted-nan": "Types are not compatible",
-  "subscripted-wrong-type": "Not an array or dictionary or set",
-  "unary-wrong-type": "Not a number",
+  "subscripted-wrong-type": "Types are not compatible",
+  "unary-wrong-type": "Types are not compatible",
   "var-incorrect-type-reassignment": "Types are not compatible",
   "var-not-declared": "Variable has not been declared",
   "var-redeclared-in-function": "Identifier already declared in this scope",
   "void-function-with-return": "Void functions cannot have return statements",
-  "wordz-assigned-to-digitz": "Types are not compatible"
+  "wordz-assigned-to-digitz": "Types are not compatible",
 };
 
 describe("The semantic analyzer", () => {
-  fs.readdirSync(__dirname).forEach(name => {
+  fs.readdirSync(__dirname).forEach((name) => {
     if (name.endsWith(".error")) {
-      test(`detected in ${name}`, done => {
+      test(`detected in ${name}`, (done) => {
         const program = parse(fs.readFileSync(`${__dirname}/${name}`, "utf-8"));
         const errorPattern = errors[name.slice(0, name.length - 6)];
         // eslint-disable-next-line no-undef
@@ -53,7 +53,7 @@ describe("The semantic analyzer", () => {
         done();
       });
     } else if (name.endsWith(".hotgirl")) {
-      test(`should analyze ${name} without errors`, done => {
+      test(`should analyze ${name} without errors`, (done) => {
         // For now, we are happy to know that these files pass semantic analysis.
         // We eventually need to check that the ASTs are properly decorated.
         const program = parse(fs.readFileSync(`${__dirname}/${name}`, "utf-8"));
